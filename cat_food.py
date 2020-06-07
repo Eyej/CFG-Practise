c3174e7adf81b3eb074ce4454264616a40e32136
Eyej <ijonyenokporo@yahoo.com>
2020-06-07 16:36:03 +0100

    Exercises and Homework from CFG class 1-4

---
+++ cat_food.py
@@ -0,0 +1,10 @@
+# cats=10
+# cans=2
+# days=7
+# total=cats*cans*days
+# output="{} cats eat {} after {} days".format(cats, total, days)
+# print(output)
+def subtract(higher, lower):
+     return higher - lower
+gain= subtract(300, 210)
+print(gain)
